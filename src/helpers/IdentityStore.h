b76f63a82b4bbcb09d5ba435d93a6e59342e2626
taco <taco@sly.nu>
2026-06-02 15:49:00 +1000

    move filesystem platform ifdefs into FILESYSTEM.h

--- src/helpers/IdentityStore.h
+++ src/helpers/IdentityStore.h
@@ -1,14 +1,6 @@
 #pragma once
 
-#if defined(ESP32) || defined(RP2040_PLATFORM)
-  #include <FS.h>
-  #define FILESYSTEM  fs::FS
-#elif defined(NRF52_PLATFORM) || defined(STM32_PLATFORM)
-  #include <Adafruit_LittleFS.h>
-  #define FILESYSTEM  Adafruit_LittleFS
-
-  using namespace Adafruit_LittleFS_Namespace;
-#endif
+#include "FILESYSTEM.h"
 #include <Identity.h>
 
 class IdentityStore {
